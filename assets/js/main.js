63716d46e539c5ec990d2529aa384d34bb903f9f
Eric Hopkins <eric@crunchbang>
2015-01-06 17:40:55 -0500

    cleaner formatting to the JS file

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,22 +3,21 @@
 
 	app.controller('PortfolioController', function() {
 		this.stocks = temp;
-		this.bank = userBank;
+		this.bank   = userBank;
 	});
 
 	// DEV: these represent dummy model data to test the controller.
 	var temp = [
-		{
-			name: 'some stock',
-			price: 34.82,
-		},
-		{
-			name: 'some other stock',
-			price: 12.4,
-		}
-	];
-
-	var userBank = 10000.00;
+            {
+                name  : 'some stock',
+                price : 34.82,
+            },
+            {
+                name  : 'some other stock',
+                price : 12.4,
+            }
+        ],
+        userBank = 10000;
 
 
 }).call(this);
