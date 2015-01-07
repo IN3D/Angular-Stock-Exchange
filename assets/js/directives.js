2354af234ce6d04f8c2bfcbe187fbcbf5f884f4f
Eric Hopkins <scarecrow3322@gmail.com>
2015-01-07 02:07:33 -0500

    refactor all major areas of the page into custom directives

--- assets/js/directives.js
+++ assets/js/directives.js
@@ -10,4 +10,18 @@
 		};
 	});
 
+	app.directive('titleBar', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'title-bar.html'
+		};
+	});
+
+	app.directive('displayArea', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'display-area.html'
+		};
+	});
+
 }).call(this);
