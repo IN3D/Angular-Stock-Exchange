1a048bf60335b432f883dd7771baa70af94b195b
Eric Hopkins <scarecrow3322@gmail.com>
2015-01-07 02:11:40 -0500

    align directive properties better

--- assets/js/directives.js
+++ assets/js/directives.js
@@ -3,24 +3,24 @@
 
 	app.directive('portfolio', function() {
 		return {
-			restrict: 'E',
-			templateUrl: 'portfolio-panel.html',
-			controller: 'PortfolioController',
-			controllerAs: 'portfolio'
+			restrict     : 'E',
+			templateUrl  : 'portfolio-panel.html',
+			controller   : 'PortfolioController',
+			controllerAs : 'portfolio'
 		};
 	});
 
 	app.directive('titleBar', function() {
 		return {
-			restrict: 'E',
-			templateUrl: 'title-bar.html'
+			restrict    : 'E',
+			templateUrl : 'title-bar.html'
 		};
 	});
 
 	app.directive('displayArea', function() {
 		return {
-			restrict: 'E',
-			templateUrl: 'display-area.html'
+			restrict    : 'E',
+			templateUrl : 'display-area.html'
 		};
 	});
 
