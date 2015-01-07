0eb05b9cbe42a7e77f2bce5aa75d3b860ede76f5
Eric Hopkins <scarecrow3322@gmail.com>
2015-01-06 20:23:55 -0500

    setup a working  call to Benzinga API

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,34 @@
 	app.controller('PortfolioController', function() {
 		this.stocks = temp;
 		this.bank   = 1000;
+
         this.giveCash = function(val) {
             this.bank += val;
         };
 	});
+
+    app.controller('SearchController', ['$http', function($http) {
+        this.searchVals = {
+            symbol : '',
+            name   : '',
+            ask    : 0,
+            bid    : 0
+        };
+
+        this.findStock = function() {
+            if (this.symbol !== '') {
+                $http.get('http://data.benzinga.com/stock/' + this.searchVals.symbol)
+                .success(function(data) {
+                    console.log(data);
+                })
+                .error(function() {
+                    console.log("http GET failed.");
+                });
+            } else {
+                alert('please enter a symbol to look up.');
+            }
+        };
+    }]);
 
 	// DEV: these represent dummy model data to test the controller.
 	var temp = [
